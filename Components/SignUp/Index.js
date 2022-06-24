fee90a9a9a1bd52c39ddfe504a09d665d1c03184
webpark-admin <admin@webpark.uz>
2022-06-24 14:54:53 +0500

    Added a sign-up form and its validation

--- Components/SignUp/Index.js
+++ Components/SignUp/Index.js
@@ -1,19 +1,60 @@
 import { SignUpField } from "./SignUpField";
+import { useFormik } from "formik";
+import { schema } from "../../validation/sign-up-validation";
 
-const SignUp = () => (
-	<form className="sign-up">
-		<h2 className="sign-up__title">Обратная связь</h2>
+const SignUp = () => {
+	const formik = useFormik({
+		initialValues: {
+			name: "",
+			email: "",
+			phone: "",
+		},
+		validationSchema: schema,
+		onSubmit: (values) => {
+			alert(JSON.stringify(values, null, 2));
+		},
+	});
 
-		<SignUpField label="Ваше имя" />
-		<SignUpField label="Ваша электронная почта" />
-		<SignUpField label="Ваш номер телефона" />
+	return (
+		<form className="sign-up" onSubmit={formik.handleSubmit}>
+			<h2 className="sign-up__title">Обратная связь</h2>
 
-		<input
-			className="sign-up__submit btn"
-			type="submit"
-			value="Отправить"
-		/>
-	</form>
-);
+			<SignUpField
+				label="*Ваше имя"
+				name="name"
+				value={formik.values.name}
+				onChange={formik.handleChange}
+				onBlur={formik.handleBlur}
+				touched={formik.touched}
+				errors={formik.errors}
+			/>
+			<SignUpField
+				label="Ваша электронная почта"
+				name="email"
+				value={formik.values.email}
+				onChange={formik.handleChange}
+				onBlur={formik.handleBlur}
+				touched={formik.touched}
+				errors={formik.errors}
+			/>
+			<SignUpField
+				label="*Ваш номер телефона"
+				name="phone"
+				value={formik.values.phone}
+				onChange={formik.handleChange}
+				onBlur={formik.handleBlur}
+				touched={formik.touched}
+				errors={formik.errors}
+				phone={true}
+			/>
+
+			<input
+				className="sign-up__submit btn"
+				type="submit"
+				value="Отправить"
+			/>
+		</form>
+	);
+};
 
 export default SignUp;
--- Components/SignUp/SignUpField.js
+++ Components/SignUp/SignUpField.js
@@ -1,6 +1,28 @@
-export const SignUpField = ({ label, props }) => (
-	<div className="sign-up-field">
-		<label className="sign-up-field__label">{label}</label>
-		<input className="sign-up-field__input" {...props} />
-	</div>
-);
+import NumberFormat from "react-number-format";
+
+export const SignUpField = ({ label, ...props }) => {
+	const { touched, errors, name, phone } = props;
+
+	return (
+		<div className="sign-up-field">
+			<label className="sign-up-field__label">{label}</label>
+
+			{phone === true ? (
+				<NumberFormat
+					className="sign-up-field__input"
+					type="text"
+					format="+(998)## ###-##-##"
+					mask="-"
+					allowEmptyFormatting
+					{...props}
+				/>
+			) : (
+				<input className="sign-up-field__input" {...props} />
+			)}
+
+			{touched[name] && errors[name] ? (
+				<p className="sign-up-field__error">{errors[name]}</p>
+			) : null}
+		</div>
+	);
+};
