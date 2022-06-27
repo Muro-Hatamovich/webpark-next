b2eebb80e422ee93aae9752c691e2d15f6ffdd3b
webpark-admin <admin@webpark.uz>
2022-06-27 05:50:07 +0500

    Added favicon and changed title

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,37 @@ import Partners from "../components/Partners";
 import Services from "../components/Services";
 import Layout from "../components/Layout";
 import { DataContext } from "../store/DataContext";
+import Head from "next/head";
 
 const Home = ({ config }) => (
 	<DataContext.Provider value={config}>
 		<Layout>
+			<Head>
+				<title>Разработка сайтов и SMM продвижение в Ташкенте</title>
+				<meta
+					name="description"
+					content="Разработка сайтов с помощью новейших технологии которые гарантируют высокую производительность и надежность, SMM продвижение..."
+				/>
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest" />
+			</Head>
+
 			<Hero />
 			<Intro />
 			<Partners />
