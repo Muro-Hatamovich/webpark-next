f509b5fa0834ddd7f0cd21b43eb57b1cd757b92d
hatamovich <tursunaliev.codes@proton.me>
2022-07-26 18:14:47 +0500

    Added faecbook-pixel-id

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-	env: {
-		EMAIL_SERVICE_ID: "service_vkhvkbo",
-		EMAIL_ACCESS_TOKEN: "R1fJOR4mt21R2AImZ",
-	},
+  env: {
+    EMAIL_SERVICE_ID: "service_vkhvkbo",
+    EMAIL_ACCESS_TOKEN: "R1fJOR4mt21R2AImZ",
+    FACEBOOK_PIXEL_ID: "468811771689236",
+  },
 };
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import { Fragment } from "react";
 import Hero from "../components/Hero";
 import Intro from "../components/Intro";
 import Partners from "../components/Partners";
@@ -8,53 +7,54 @@ import { DataContext } from "../store/DataContext";
 import Head from "next/head";
 
 const Home = ({ config }) => (
-	<DataContext.Provider value={config}>
-		<Layout>
-			<Head>
-				<title>Разработка сайтов и SMM продвижение в Ташкенте</title>
-				<meta
-					name="description"
-					content="Разработка сайтов с помощью новейших технологии которые гарантируют высокую производительность и надежность, SMM продвижение..."
-				/>
-				<link
-					rel="apple-touch-icon"
-					sizes="180x180"
-					href="/apple-touch-icon.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="32x32"
-					href="/favicon-32x32.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="16x16"
-					href="/favicon-16x16.png"
-				/>
-				<link rel="manifest" href="/site.webmanifest" />
-			</Head>
+  <DataContext.Provider value={config}>
+    <Layout>
+      <Head>
+        <title>Разработка сайтов и SMM продвижение в Ташкенте</title>
+        <meta
+          name="description"
+          content="Разработка сайтов с помощью новейших технологии которые гарантируют высокую производительность и надежность, SMM продвижение..."
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
 
-			<Hero />
-			<Intro />
-			<Partners />
-			<Services />
-		</Layout>
-	</DataContext.Provider>
+      <Hero />
+      <Intro />
+      <Partners />
+      <Services />
+    </Layout>
+  </DataContext.Provider>
 );
 
 export const getStaticProps = async (props) => {
-	return {
-		props: {
-			config: {
-				service_id: process.env.EMAIL_SERVICE_ID,
-				access_token: process.env.EMAIL_ACCESS_TOKEN,
-			},
+  return {
+    props: {
+      config: {
+        service_id: process.env.EMAIL_SERVICE_ID,
+        access_token: process.env.EMAIL_ACCESS_TOKEN,
+        pixel_id: process.env.FACEBOOK_PIXEL_ID,
+      },
 
-			revalidate: 60000,
-		},
-	};
+      revalidate: 60000,
+    },
+  };
 };
 
 export default Home;
